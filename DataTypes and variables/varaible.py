0a56f97ae6e365eb41430ce4e0ebd40cde69a466
Syed Alam Shah Bukhari <161896817+syedalamshah@users.noreply.github.com>
2025-12-30 02:14:42 +0500

    Variables

---
+++ DataTypes and variables/varaible.py
@@ -0,0 +1,8 @@
+first_name = "Alam"
+last_name = "Shah"
+age = 30    
+is_student = False
+height = 5.9
+print("First Name:", first_name)
+print("Last Name:", last_name)              
+print ("age:" , age)
