defe41b279f007240dd72774c3dc187d3ba184bb
Syed Alam Shah Bukhari <161896817+syedalamshah@users.noreply.github.com>
2025-12-30 02:29:16 +0500

    changes

--- DataTypes and variables/varaible.py
+++ DataTypes and variables/varaible.py
@@ -1,8 +1,13 @@
+# Simple examples for variables and data types
+
 first_name = "Alam"
 last_name = "Shah"
-age = 30    
+age = 30
 is_student = False
 height = 5.9
+
 print("First Name:", first_name)
-print("Last Name:", last_name)              
-print ("age:" , age)
+print("Last Name:", last_name)
+print("Age:", age)
+print("Is Student:", is_student)
+print("Height:", height)
