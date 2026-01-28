341d5ae39bdd9d538411df22d9499e8b77c91beb
Syed Alam Shah Bukhari <161896817+syedalamshah@users.noreply.github.com>
2026-01-29 01:06:14 +0500

    if else

---
+++ Operators/conditional.py
@@ -0,0 +1,6 @@
+if "alam is learning python":
+    print("yes")
+elif "alam is learning java":
+    print("maybe")
+else:
+    print("no")
---
+++ Operators/score.py
@@ -0,0 +1,8 @@
+Score = 70 ;
+
+if Score > 90 :
+    print("A+")
+elif Score > 80 :
+    print("A")
+else :
+    print("fail") ;
